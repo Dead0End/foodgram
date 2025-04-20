e6c4ba937e2cc26497e15dc6985cc9fcb8000ab4
DeadEnd <mbichizhik@gmail.com>
2025-04-20 20:41:36 +0300

    fix

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -285,26 +285,26 @@ class RecipeIngredient(models.Model):
     )
 
 
-#class ShoppingCart(models.Model):
-#    user = models.ForeignKey(
-#        User,
-#        on_delete=models.CASCADE,
-#        related_name='shopping',
-#        verbose_name='Пользователь',
-#    )
-#    recipe = models.ForeignKey(
-#        Recipe,
-#        on_delete=models.CASCADE,
-#        related_name='in_shopping_cart',
-#        verbose_name='название рецепта',
-#    )
-#
-#    class Meta:
-#        constraints = [
-#            models.UniqueConstraint(
-#                fields=['user', 'recipes'],
-#                name='unique_shopping_cart'
-#            )
-#        ]
-#        verbose_name = 'Список покупок'
-#        verbose_name_plural = 'Списки покупок'
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='shopping',
+        verbose_name='Пользователь',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='in_shopping_cart',
+        verbose_name='название рецепта',
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipes'],
+                name='unique_shopping_cart'
+            )
+        ]
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
