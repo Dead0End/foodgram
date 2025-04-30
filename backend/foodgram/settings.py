f78cebea6f84fae74510bbb0ce17926e2429a8b0
DeadEnd <mbichizhik@gmail.com>
2025-04-30 23:47:44 +0300

    fix media

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/html/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
