9230a6f6cfd6af8fe2f3094c55104e0a0783101f
DeadEnd <mbichizhik@gmail.com>
2025-05-15 18:55:17 +0300

    fix recipes  admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,44 +1,50 @@
-from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.db.models import Count
 
 from users.models import Follower, CustomUser
 from recipes.models import Recipe, Ingredient, Tag
 
 
-class Admin(UserAdmin):
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
     list_display = (
         'id',
         'username',
         'email',
         'first_name',
         'last_name',
-        'avatar')
-    ordering = ('id')
-
-
-class AdminFollower(Follower):
-    list_display = ('user', 'following')
-    search_fields = ('user', 'following')
-
-
-@admin.register(CustomUser)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+        'avatar'
+    )
     list_display_links = ('id', 'username')
     search_fields = ('username', 'email')
     list_filter = ('is_staff', 'is_active', 'is_superuser')
+    ordering = ('id',)
+    list_per_page = 25
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+@admin.register(Follower)
+class FollowerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'following')
+    list_display_links = ('id', 'user')
+    search_fields = ('user__username', 'following__username')
     list_per_page = 25
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'author', 'favorites_count', 'pub_date')
+    list_display = ('id', 'name', 'author', 'favorites_count', 'created_at')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'author__username', 'author__email')
     list_filter = ('tags',)
     filter_horizontal = ('tags',)
-    readonly_fields = ('favorites_count',)
+    readonly_fields = ('favorites_count', 'created_at')
     list_per_page = 25
 
     def get_queryset(self, request):
