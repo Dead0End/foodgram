e134d93d3460ad89c3cbb5085e0508fe19725ccf
DeadEnd <mbichizhik@gmail.com>
2025-05-18 01:37:19 +0300

    fix imports

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,7 +1,7 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.core.exceptions import ValidationError
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
 
 from .constants import (
     TAG_NAME_MAX_LENGTH,
