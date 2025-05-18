33431ff820d5cc63dd8e03e9705c3808d21389d5
DeadEnd <mbichizhik@gmail.com>
2025-05-19 00:37:00 +0300

    правки filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,12 +1,12 @@
 from django_filters.rest_framework import FilterSet, filters
-
-from recipes.models import Ingredient, Recipe
-
+from recipes.models import Ingredient, Recipe, Tag
+from users.models import User
 
 class IngredientFilter(FilterSet):
     name = filters.CharFilter(
         field_name='name',
-        lookup_expr='istartswith')
+        lookup_expr='istartswith'
+    )
 
     class Meta:
         model = Ingredient
@@ -14,6 +14,25 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
+
     class Meta:
         model = Recipe
-        fields = ('tags__slug', 'author')
+        fields = ('tags', 'author')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(shopping_carts__user=self.request.user)
+        return queryset
