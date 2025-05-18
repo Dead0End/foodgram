b7c541c7b678233b539fa2960261ba8f4fc6dd83
DeadEnd <mbichizhik@gmail.com>
2025-05-19 01:07:33 +0300

    fix pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,13 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
-
-class Pagination(PageNumberPagination):
-    page_size = 5
-    max_page_size = 20
+class CustomPagination(PageNumberPagination):
+    page_size = 6
     page_size_query_param = 'limit'
-
-
-class IngredientResultsPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 20
