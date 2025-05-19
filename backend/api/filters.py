db9dddca5cfc97082d2199afba16fba876e6d5f8
DeadEnd <mbichizhik@gmail.com>
2025-05-19 08:32:49 +0300

    fix flake

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -25,7 +25,8 @@ class RecipeFilter(FilterSet):
     )
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
